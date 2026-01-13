346c78941804d596fb725319e26b433094604931
Krishna Kumar <krishna.kumar@logicvalley.in>
2026-01-13 18:02:40 +0530

    Updates in page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.intro}>
-          <h1>Automatically Deploy Your App</h1>
+          <h1>Automatically Deploy Your App on Azure</h1>
         </div>
         <div className={styles.ctas}>
           <a className={styles.primary} onClick={deploy}>
